a3f29f2cd0d73cbe741526c16abc8bdba4bf959d
Ikenna Omeje <karl.ikenna@outlook.com>
2024-12-28 20:50:56 +0000

    created a form to get info from html and opened a backend server booking.js

--- booking.js
+++ booking.js
@@ -1,21 +1,29 @@
-const express = require("express");
+//const express = require("express");
+import express from "express";
 const app = express();
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
+app.use(express.static("public"));
 
 //in-memory storage
 const availableDates = ["2024-01-15", "2024-01-20", "2024-02-10"];
 const bookings = [];
 
+// add route end-point
+app.get("/", (_req, res) => {
+    res.send("Welcome to the Backend Server!");
+});
+
 //end-point to fetch available dates
-app.get("/api/available-dates", (req, res) => {
+app.get("/api/available-dates", (_req, res) => {
     res.json({ dates: availableDates });
 });
 
 // end-point to handle bookings
 app.post("/api/book-date", (req, res) => {
     const { date, name, email } = req.body;
+    
 //validate date
     if (!availableDates.includes(date)) {
         return res.status(400).json({ message: "Selected date is not available."});
